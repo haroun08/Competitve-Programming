4ca8b44a6b5af8f4e3a04381a13529d9d911c9d8
Haroun Barhoumi <haroun.barhoumi@etudiant-enit.utm.tn>
2025-02-20 22:30:17 +0100

    Find Unique Binary String.cpp

---
+++ LeetCode/Find Unique Binary String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string result ="";
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            result  += curr == '0' ? '1' : '0';
+        }
+        
+        return result ;
+    }
+};
