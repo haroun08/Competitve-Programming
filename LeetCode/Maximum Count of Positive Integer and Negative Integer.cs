a2a81d8651f5af3341f02b72de38f1590be25750
Haroun Barhoumi <haroun.barhoumi@etudiant-enit.utm.tn>
2025-03-12 22:04:41 +0000

    Maximum Count of Positive Integer and Negative Integer.cs

---
+++ LeetCode/Maximum Count of Positive Integer and Negative Integer.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaximumCount(int[] nums) {
+        int pos = 0 ,neg = 0;
+        foreach(int num in nums){
+            if(num>0){
+                pos++;
+            }
+            else if(num<0) neg++;
+        }
+        return pos>neg ? pos : neg;
+    }
+}
